7fce2c5e8ee4fa6523a51ff8f4e3f7a6631f00a4
Alexander <alexanderk9@mail.ru>
2025-04-16 13:18:47 +0300

    Добавил файл settings.py с переменными для чат-бота: данные клиента, компании и суммы для звонка

--- settings.py
+++ settings.py
@@ -1,4 +1,16 @@
+# settings.py
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    pass
+    # Данные клиента 
+    FULL_NAME: str = "Степанчук Алена Сергеевна"
+    ACCOUNT_NUMBER: str = "111222333"
+    DEBT_AMOUNT: str = "24 760 руб."
+    ADDRESS: str = "Москва, Сокольнический вал, д. 20/3, кв. 3"
+
+    # Данные компании
+    COMPANY_NAME: str = "ЭнергоСбыт"
+    COMPANY_PHONE: str = "8-900-555-55-55"
+
+    # Сумма частичной оплаты (50% от DEBT_AMOUNT)
+    PARTIAL_PAYMENT_AMOUNT: str = "12 380 руб."
